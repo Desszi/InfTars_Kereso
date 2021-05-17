36802a507abaf5587dc68cdfffbb238eae50dc9a
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2021-05-17 17:05:57 +0200

    Váza az alkalmazásnak

--- SearchPage/src/app/app.module.ts
+++ SearchPage/src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+import { SimpleSearchComponent } from './page/simple-search/simple-search.component';
+import { ComplexSearchComponent } from './page/complex-search/complex-search.component';
+import { SortPipe } from './pipes/sort.pipe';
+import { SearchPipe } from './pipes/search.pipe';
+import { JournalService } from './service/journal.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SimpleSearchComponent,
+    ComplexSearchComponent,
+    SortPipe,
+    SearchPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [JournalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
