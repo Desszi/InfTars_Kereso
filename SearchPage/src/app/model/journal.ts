0be61ca2e635b86e1d0ce977307e87cfc2761994
Mercédesz Anett Porkoláb <09dporkolabm@edu.bme.hu>
2021-05-17 16:24:24 +0200

    Journal osztály megvalósítása

---
+++ SearchPage/src/app/model/journal.ts
@@ -0,0 +1,16 @@
+export class Journal {
+
+    id: string  = "";
+    ev: string = "";
+    evfolyam: string = "";
+    lapszam: string = "";
+    rovat: string = "";
+    szerzok: string ="";
+    forditok: string = "";
+    cim: string ="";
+    eredeticim: string ="";
+    oldal: string = "";
+    kulcsszavak: string ="";
+    doi: string = "";
+    url: string ="";
+}
